12a44389004a57195aab2b097590e39da3ae8188
baekexsoo <ndamarina17@gmail.com>
2021-05-20 12:06:18 +0100

    first commit

---
+++ src/app/acheteurs/acheteurs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-acheteurs',
+  templateUrl: './acheteurs.component.html',
+  styleUrls: ['./acheteurs.component.css']
+})
+export class AcheteursComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
